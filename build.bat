2f60aae6905e5fb49780fcac52f3031f7ec77a50
Louis Mazin <mazinlouis79@gmail.com>
2024-10-10 14:15:25 +0200

    update

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-pipenv run pyinstaller --onefile --clean --noconsole --distpath "../Build" --specpath "../.Building" --name "Musiques Youtube" --icon "../Sources/icon.ico" --workpath "../.Building" --add-data "../Sources/ffmpeg.exe;." --add-data "../Sources/ffprobe.exe;." Main.py
+call ./youtube_musique\Scripts\activate.bat
+pyinstaller --onefile --clean --noconsole --distpath "../Build" --specpath "../.Building" --name "Musiques Youtube" --icon "../Sources/icon.ico" --workpath "../.Building" --add-data "../Sources/ffmpeg.exe;." --add-data "../Sources/ffprobe.exe;." Main.py
